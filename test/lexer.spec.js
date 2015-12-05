63c2b87cc2389b89cd43c62f42630d2b53f9d0aa
Pete Redmond <redmondp@gmail.com>
2015-12-05 16:08:11 +0000

    (feat) tokenize headers
    part of the tokenise method, lex a header into a token if it is the
    valid markdown syntax for headers

--- test/lexer.spec.js
+++ test/lexer.spec.js
@@ -18,4 +18,30 @@ describe('Lexer class', function() {
     expect(lexer.rules).to.be.an('object');
   });
 
+  describe('tokenize method', function() {
+
+    describe('Header component', function() {
+
+      it('should return a valid "Heading" token', function() {
+        var input = '# hello world';
+        var result = lexer.tokenize(input);
+        var token = result[0];
+
+        expect(result.length).to.equal(1);
+        expect(token.type).to.equal('Heading');
+        expect(token.value).to.equal('hello world');
+      });
+
+      it('should not accept a invalid heading', function() {
+        var input = '#abcshbschbs';
+        var result = lexer.tokenize(input);
+
+        expect(result.length).to.equal(0);
+      });
+
+    });
+
+
+  });
+
 });
